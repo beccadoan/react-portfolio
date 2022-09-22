d7ad8cf06e90852db436264b93084212d6c12ac1
Becca Doan <rebecca.doan@yahoo.com>
2022-09-22 14:55:22 -0500

    added Contact section and conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
+import React, { useState } from 'react';
 import './App.css';
 import Nav from './components/Nav'
 import Portfolio from './components/Portfolio'
 import About from './components/About'
 import Footer from './components/Footer'
-
+import ContactForm from './components/Contact'
 
 function App() {
+  const [currentCategory, setCurrentCategory] =useState('About')
   return (
     <body>
       <div className="content">
-        <Nav/>
+        <Nav 
+        currentCategory={currentCategory}
+        setCurrentCategory={setCurrentCategory}/>
         <main>
-          <About />
-          <Portfolio />
+          {currentCategory === 'About' && <About />}
+          {currentCategory === 'Portfolio' && <Portfolio />}
+          {currentCategory === 'Contact' && <ContactForm />}
         </main>
       </div>
       <Footer />
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,24 +1,36 @@
 import React from 'react';
 
-function Nav(){
+function Nav({currentCategory, setCurrentCategory}){
     return (
         <header>
         <h1>
-            <a href="/">Becca's Portfolio</a>
+            <span><a href="/">Becca's Portfolio</a></span>
         </h1>
         <nav>
             <ul>
                 <li>
-                    <a href="/">About Me</a>
+                    <span 
+                    className={`${currentCategory === 'About' && 'nav-active'}`}
+                    onClick={()=>setCurrentCategory('About')}>About Me</span>
                 </li>
                 <li>
-                    <a href="/">Portfolio</a>
+                    <span 
+                    className={`${currentCategory === 'Portfolio' && 'nav-active'}`}
+                    onClick={()=>setCurrentCategory('Portfolio')}>
+                    Portfolio</span>
                 </li>
                 <li>
-                    <a href="/">Contact Me</a>
+                    <span 
+                    className={`${currentCategory === 'Contact' && 'nav-active'}`}
+                    onClick={()=>setCurrentCategory('Contact')}>
+                    Contact Me
+                    </span>
                 </li>
                 <li>
-                    <a href="/">Resume</a>
+                    <span 
+                    className={`${currentCategory === 'Resume' && 'nav-active'}`}
+                    onClick={()=>setCurrentCategory('Resume')}>
+                        Resume</span>
                 </li>
             </ul>
         </nav>
